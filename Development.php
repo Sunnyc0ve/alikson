77b9891d27fd84ec2f9d6397a4dd454107e11418
Dex1502 <Dexter1502@yandex.ru>
2025-03-04 15:01:05 +0300

    р

--- Development.php
+++ Development.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="css/header.css" />
-    <link rel="stylesheet" href="css/pages/development/_development.css" />
+    <link rel="stylesheet" href="css/pages/development/_development.css?1.0" />
     <link rel="stylesheet" href="fonts/stylesheet.css" />
     <link
       rel="stylesheet"
