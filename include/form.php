bd510e5318878224353c85e61379c154904b6136
breaz1 <breaz1@yandex.ru>
2025-07-03 22:53:40 +0500

    apart +- final

--- include/form.php
+++ include/form.php
@@ -1,4 +1,4 @@
-<section class="FormSection" id="AnchorForm">
+  <section class="FormSection" id="AnchorForm">
     <img class="deskFormImg" src="img/FormBack.png">
     <div class="Form-Header">
       <p>Оставьте заявку и получите бесплатную консультацию уже сегодня!</p>
@@ -6,11 +6,18 @@
       <form action="" method="POST">
         
           <p class="FormHeaderMobile">Оставьте заявку и получите бесплатную консультацию уже сегодня!</p>
-          <input type="text" name="name" required placeholder="Ваше имя" />
-          <input type="text" name="phone" required placeholder="Ваш телефон" />
+          <input type="text" name="name" required="" placeholder="Ваше имя">
+          <input
+            id="phone"
+            type="tel"
+            name="phone"
+            placeholder="+7 (___) ___-__-__"
+            required
+            pattern="^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$"
+          />
         <div class="FormButtonContainer">
         <span>Оставить заявку</span>
-        <input type="submit" value="Оставить заявку" />
+        <input type="submit" value="Оставить заявку">
         </div>
         <p>Мы&nbsp;свяжемся с&nbsp;вами в&nbsp;ближайшее время, чтобы ответить на&nbsp;все вопросы</p>
       </form>
